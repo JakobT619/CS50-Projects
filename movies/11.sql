b747b4b79171359a95a8f66501eed6c139258f1a
JakobT619 <jakob.t97@hotmail.com>
2025-03-17 18:10:38 -0500

    Update 11.sql

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,7 @@
+In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
+Your query should output a table with a single column for the title of each movie.
+You may assume that there is only one person in the database with the name Chadwick Boseman.
+
 SELECT DISTINCT title FROM movies
 JOIN ratings ON  movies.id = ratings.movie_id
 JOIN stars ON ratings.movie_id = stars.movie_id
