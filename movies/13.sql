78ba8d3d3100dfb052175f2e84ec539778f1d79f
JakobT619 <jakob.t97@hotmail.com>
2025-03-17 18:11:59 -0500

    Update 13.sql

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,8 @@
+In 13.sql, write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
+Your query should output a table with a single column for the name of each person.
+There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
+Kevin Bacon himself should not be included in the resulting list.
+
 SELECT DISTINCT p2.name FROM people p1
 JOIN stars s1 ON p1.id = s1.person_id
 JOIN movies ON s1.movie_id = movies.id
