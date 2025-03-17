a6a7f200e27323a620d460bd35fef6e4ee225c8d
JakobT619 <jakob.t97@hotmail.com>
2025-03-17 18:11:09 -0500

    Update 12.sql

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,8 @@
+In 12.sql, write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+Your query should output a table with a single column for the title of each movie.
+You may assume that there is only one person in the database with the name Bradley Cooper.
+You may assume that there is only one person in the database with the name Jennifer Lawrence.
+
 SELECT title FROM movies
 JOIN stars s1 ON movies.id = s1.movie_id
 JOIN people p1 ON s1.person_id = p1.id
